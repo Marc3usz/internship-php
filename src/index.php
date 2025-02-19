f3521f88f6b1cc2e3a9c4855ebe6bba9e7816ce3
Marcel Geba <gibbon.master@gmail.com>
2025-02-19 15:02:48 +0100

    test database select statement + fixing dockerfile for PDO

--- src/index.php
+++ src/index.php
@@ -1,3 +1,22 @@
 <?php
-	echo "Test";
+	$host= "127.0.0.1";
+ 	$dbname= "test_db";
+	$username= "user";
+ 	$password= "user_password";
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    
+    
+    $stmt = $pdo->query("SELECT * FROM users");
+    
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo "ID: " . $row['id'] . "<br>";
+        echo "Name: " . $row['name'] . "<br>";
+        echo "Email: " . $row['email'] . "<br><br>";
+    }
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+$pdo = null;
 ?>
