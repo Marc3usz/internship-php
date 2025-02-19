119c8e94b01f644725b1bcb49ce30bbf9ae92446
Marcel Geba <gibbon.master@gmail.com>
2025-02-19 14:57:47 +0100

    Added initialization script for mysql to the docker container

---
+++ init-mysql/init.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS my_database;
+USE my_database;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    is_admin TINYINT(1) NOT NULL DEFAULT 0, -- 0 is normal user, 1 is admin
+);
+
+INSERT INTO users (name, email, is_admin)
+VALUES ('Administrator', 'admin@admin.com', 1);
